9bebac84a7c9a4639bdf4a7a695c11cb4a62f7e2
unknown <jaggers.tyler@gmail.com>
2026-07-21 16:48:29 -0700

    User window separated

--- MAIN.py
+++ MAIN.py
@@ -1,23 +1,6 @@
-from live_frame import pipCount
-from tkinter import*
-from tkinter import ttk
+from interface import userWindow
 
 def main():
-    root = Tk()
-    frm = ttk.Frame(root, padding=10)
-    frm.grid()
-
-    pip_text = StringVar()
-    pip_text.set(pipCount())
-
-    ttk.Label(frm, textvariable=pip_text).grid(column=0, row=0)
-    ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=3)
-   
-    def refresh():
-        pip_text.set(pipCount())
-        root.after(500, refresh)
-
-    root.after(500, refresh)
-    root.mainloop()
+    userWindow()
 
 main()
