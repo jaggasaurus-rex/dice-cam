f635f2c81842fbceeed5a13774f34975d1595842
jaggasaurus-rex <jaggers.tyler@gmail.com>
2026-07-21 17:34:47 -0700

    frame error handling

--- interface.py
+++ interface.py
@@ -4,17 +4,22 @@ from tkinter import ttk
 
 def userWindow():
     root = Tk()
+    root.geometry("800x600")
     frm = ttk.Frame(root, padding=10)
     frm.grid()
 
     pip_text = StringVar()
-    pip_text.set(pipCount())
+    pip_text.set("")
 
-    ttk.Label(frm, textvariable=pip_text).grid(column=0, row=0)
-    ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=3)
+    root.columnconfigure(0, weight=1)
+
+    ttk.Label(frm, textvariable=pip_text, font=("Helvetica",120,"bold")).grid(column=0, row=0,columnspan=3,sticky="",padx=10)
+    ttk.Button(frm, text="Quit", command=root.destroy).grid(column=3, row=1,sticky="e")
    
     def refresh():
-        pip_text.set(pipCount())
+        pips_check = pipCount()
+        if pips_check:
+            pip_text.set(pips_check)
         root.after(500, refresh)
 
     root.after(500, refresh)
