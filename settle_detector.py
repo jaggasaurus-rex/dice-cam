80e05a9e0ee8d5b9c7ab6326be3f80e586b082c3
unknown <jaggers.tyler@gmail.com>
2026-07-29 13:42:52 -0700

    separating some resuable function

--- settle_detector.py
+++ settle_detector.py
@@ -6,28 +6,34 @@ import time
 
 capture = cv2.VideoCapture(0, apiPreference=cv2.CAP_ANY)
 
+def frameConversion(frame):
+    bw = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(bw, (5,5), 0)
+    return blur
+
+def frameDiff(active, prev):
+    diff = cv2.absdiff(active,prev)
+    result = cv2.mean(diff, mask=None)
+    return result[0]
+
 def frameNoise():
     prev = None
     samples = []
     counter = 0
     active = True
+    threshhold = 0.0
     while active == True:
         ret, frame = capture.read()
         if ret is False:
             break
-        bw = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        blur = cv2.GaussianBlur(bw, (5,5), 0)
+        blur = frameConversion(frame)
         if prev is not None:
-            diff = cv2.absdiff(blur, prev)
-            result = cv2.mean(diff, mask=None)
-            cv2.imshow("dice came", blur)
-            if cv2.waitKey(1) & 0xFF == ord('q'): 
-                break
-            if result[0] == 0:
+            result = frameDiff(blur, prev)
+            if result == 0:
                 continue
             counter+=1
             if counter >= 30:
-                samples.append(result[0])
+                samples.append(result)
             
         prev = blur
 
@@ -35,7 +41,18 @@ def frameNoise():
             mean_frame_noise = statistics.mean(samples) + 4 * statistics.stdev(samples)
             threshhold = mean_frame_noise * error_margin
             active = False
-            return threshhold
+    print(threshhold)
+    return threshhold
+
+
+def frameNoiseTest():
+    threshhold = frameNoise()
+    
+    while True:
+        ret, frame = capture.read()
+        if ret is False:
+            raise Exception("Camera read error")
+        cv2.imshow("dice came", frame)
 
 
 def rollDetector():
@@ -85,5 +102,6 @@ def rollDetector():
             quiet_since = None
 """
 
-rollDetector()
+frameNoiseTest()
+#frameNoise()
 
