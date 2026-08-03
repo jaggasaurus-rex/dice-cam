260e42b3a721472ee690807ea0dd103ba3675da1
unknown <jaggers.tyler@gmail.com>
2026-08-02 22:11:59 -0700

    file cleanup and separation

--- settle_detector.py
+++ settle_detector.py
@@ -4,7 +4,7 @@ from general_variables import *
 import time
 from config import *
 from camera import capture
-import numpy as np
+from frame_initialization import *
 
 
 def frameConversion(frame):
@@ -94,83 +94,3 @@ def dieRollDetection(threshold, roi):
 
         prev = current_frame
 
-def selectRoi():
-    counter = 0
-    active = True
-    while active == True:
-        ret, frame = capture.read()
-        if ret is False:
-            raise Exception("Camera read error")
-        counter+=1
-        if counter >= 30:
-            roi = cv2.selectROI("Calibration Window", frame, showCrosshair=True, fromCenter=False)
-            if roi[2] == 0 or roi[3] == 0: #user cancelled
-                return None
-            
-            active = False
-
-            return [int(v) for v in roi]
-
-def firstRunROIConfig():
-    cfg = loadConfig()
-    if cfg["roi_points"] is not None:
-        return cfg
-    while True:
-        points = multiPoint()
-        if points is not None:
-            break    
-        print("Tray outline required - please click at lest 3 points")  # user cancelled - re-prompting
-
-    cfg = writeEntryToConfig("roi_points", points)
-    cv2.destroyAllWindows()
-    return cfg
-
-
-def occupancyCount(processed_frame, background):
-    diff = cv2.absdiff(processed_frame, background)
-    _, mask = cv2.threshold(diff, 25, 255, cv2.THRESH_BINARY)
-    return mask, cv2.countNonZero(mask)
-
-def grabProcessedFrame(frame, roi):
-    cropped = cropToRoi(frame, roi)
-    processed = frameConversion(cropped)
-    return processed
-
-def onMouse(event, x, y, flags, param):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        param.append([x, y])
-
-def multiPoint():
-    ret, frame = capture.read()
-    if ret is False:
-        raise Exception("Camera read error")
-    points = []
-    cv2.namedWindow("dice cam")
-    cv2.setMouseCallback("dice cam", onMouse, param=points)
-    while True:
-        display = frame.copy()
-
-        for p in points:
-            cv2.circle(display, tuple(p), 4, (0, 255, 0), -1)
-        if len(points) >= 2:
-            cv2.polylines(display, [np.array(points, dtype=np.int32)], False, (0, 255, 0), 2)
-        if len(points) >= 2:
-                    cv2.polylines(display, [np.array(points, dtype=np.int32)], True, (0, 255, 0), 2)
-        
-
-        cv2.imshow("dice cam", display)
-
-        key = cv2.waitKey(1) & 0xFF    #Enter
-        if key == 13 and len(points) >= 3:
-            break
-        elif key == ord('z') and points:  #undo
-            points.pop()
-        elif key == 27:       #esc
-            cv2.destroyWindow("dice cam")
-            return None
-
-    cv2.destroyWindow("dice cam")
-    return points
-
-
-firstRunROIConfig()
